7793820270e6418f73eaabfb715154212e2e8dc2
Heiko Dreyer <mail@boxedfolder.com>
2013-07-11 10:57:27 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qtrack::Application.config.secret_token = 'bc07e3f49ed0d6410af382c1a39bd131cd5dfb4893dfa284f11db2ee957e9ddacdb47ce0fa3f767c96e37e019e88e9a414168e3e8ed87e7b59323acc71346548'
