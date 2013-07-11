72ed883488498a01f44054962094d24a4be0708a
Heiko Dreyer <mail@boxedfolder.com>
2013-07-11 20:46:02 +0200

    Basic login - Routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+
+  layout :layout_by_resource
+
+  def after_sign_out_path_for(resource_or_scope)
+    # Little hard coded
+    '/users/sign_in'
+  end
+
+protected
+  def layout_by_resource
+    if devise_controller?
+      'login'
+    else
+      'application'
+    end
+  end
 end
