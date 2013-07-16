e37687849b79c1866f220dde5d9df6bd5357ada9
Heiko Dreyer <mail@boxedfolder.com>
2013-07-16 14:52:49 +0200

    Routing update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Qtrack::Application.routes.draw do
   devise_for :users
 
-  resources :comments
-  resources :issues
-  resources :projects
+  resources :projects do
+    resources :issues, shallow: true do
+      resources :comments
+    end
+  end
 
   root :to => redirect('/projects')
 end
