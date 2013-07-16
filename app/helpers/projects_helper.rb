53e6e4e108df94b85610151428c731fd7bcabbab
Heiko Dreyer <mail@boxedfolder.com>
2013-07-16 10:36:31 +0200

    Added some helpers / worked on projects list

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,6 @@
 module ProjectsHelper
+  def print_issue_counter(project)
+      a_spell = project.issues.size == 1 ? 'issue' : 'issues'
+      "#{project.issues.size} #{a_spell} - #{project.issues.select{|i| i.is_resolved == false}.size} open"
+  end
 end
